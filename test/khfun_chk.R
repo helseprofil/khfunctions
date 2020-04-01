4c55fd9eafa9398a99c313dfa09e46076efaf3e0
YBK <ybkamaleri@users.noreply.github.com>
2020-04-01 12:23:28 +0200

    Extract raw data from Access tabel
    This if need to create a new table

--- test/khfun_chk.R
+++ test/khfun_chk.R
@@ -12,10 +12,10 @@ rdsPath <- "F:/Prosjekter/Kommunehelsa/PRODUKSJON/PRODUKTER/MELLOMPROD/R/STABLAO
 ## Path for linux
 rdsPath <- "/f/Prosjekter/Kommunehelsa/TESTOMRAADE/TEST_KHFUN/DBtest/PRODUKTER/MELLOMPROD/R/STABLAORG/NYESTE"
 
-## From Excel file width format
-testFil <- "UFORE.rds"
+## ## From Excel file width format
+## testFil <- "UFORE.rds"
 
-testFil <- "ELEVUNDER.rds"
+## testFil <- "ELEVUNDER.rds"
 
 
 ## CSV files
@@ -23,7 +23,7 @@ filnavn <- c("ELEVUNDER", "ARBLEDIGE", "LaVINNT_1G")
 testFil <- paste(filnavn, "rds", sep = ".")
 
 
-df <- readRDS(file.path(rdsPath, testFil[1]))
+df <- readRDS(file.path(rdsPath, testFil[2]))
 
 library(data.table)
 setDT(df)
@@ -61,3 +61,68 @@ dtSub <- c(valgEnh, "Aremark","Hasvik", "Grong") #med Prikket
 dt <- DF[DF[,.I[1:5],by=.(Enhetsnavn)]$V1][Enhetsnavn  %in% dtSub, ]
 
 fwrite(dt, paste(rawPath, "testPrikk.csv", sep = "\\"), sep = ";")
+
+
+
+## Extract Original files with file types
+## --------------------------------------
+
+dbfile <- "c:/enc/DBtest/STYRING/KHELSA_dev.mdb"
+
+
+library(RODBC)
+library(glue)
+library(DBI)
+library(data.table)
+
+innTb <- "INNLESING"
+orgTb <- "ORIGINALFILER"
+kobTb <- "ORGINNLESkobl"
+
+
+khcon <- RODBC::odbcConnectAccess2007(dbfile)
+
+## Get all data at once
+innDT <- setDT(RODBC::sqlFetch(khcon, innTb))
+orgDT <- setDT(RODBC::sqlFetch(khcon, orgTb))
+kobDT <- setDT(RODBC::sqlFetch(khcon, kobTb))
+
+lapply(list(inn = innDT, org = orgDT, kob = kobDT), names)
+lapply(list(inn = innDT, org = orgDT, kob = kobDT), dim)
+
+
+kobDT[duplicated(FILID), .N]
+kobDT[duplicated(KOBLID), .N]
+
+## check if there is duplicated
+kobDT[, id := paste(FILID, FILGRUPPE, DELID, sep = "_")]
+kobDT[duplicated(id), .N]
+kobDT[duplicated(id), ] ## only missing has duplicated
+
+
+innCols <- c("FILGRUPPE", "DELID", "RSYNT1")## Select specific
+
+
+
+
+
+
+
+
+## Give ORIGINALFILER
+
+
+
+
+
+
+
+sqlGet <- glue_sql("
+  SELECT {cols*}
+  FROM {innles}",
+  .con = DBI::ANSI()
+  )
+
+khdf <- RODBC::sqlQuery(khcon, sqlGet)
+
+odbcClose(khcon)
