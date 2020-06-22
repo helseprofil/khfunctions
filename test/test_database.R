ce1322f49093a74cfea3513d9d5b52eb6600f383
YBK <ybkamaleri@users.noreply.github.com>
2020-06-22 16:49:07 +0200

    Add test DB

--- test/test_database.R
+++ test/test_database.R
@@ -13,3 +13,37 @@ test <- function(){
 }
 
 test()
+
+
+
+## TEST
+library(RODBC)
+## Set up driver info and database path
+DRIVERINFO <- "Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
+MDBPATH <- "C:\\enc\\DBtest\\STYRING\\KHELSA_dev2.accdb"
+PATH <- paste0(DRIVERINFO, "DBQ=", MDBPATH)
+
+## Establish connection
+channel <- odbcDriverConnect(PATH)
+
+## Load data into R dataframe
+df <- sqlQuery(channel,
+"SELECT [student_id], [first_name], [last_name],
+FROM [tbl-students]
+ORDER BY [first_name];",
+stringsAsFactors = FALSE)
+
+## Close and remove channel
+close(channel)
+rm(channel)
+
+
+## With odbc and DBI
+pkg <- c("odbc", "DBI")
+sapply(pkg, require, character.only = TRUE)
+
+db_con <- "Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq="
+MDBPATH <- "C:\\enc\\DBtest\\STYRING\\KHELSA_dev2.accdb"
+
+cs <- paste0(db_con, MDBPATH)
+con <- dbConnect(odbc::odbc(), .connection_string = cs)
