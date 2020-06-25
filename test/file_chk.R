cb9e5b8bbd0ba1ec92a2700baf174740e1b407d9
YBK <ybkamaleri@users.noreply.github.com>
2020-06-25 14:55:09 +0200

    Geo tables

--- test/file_chk.R
+++ test/file_chk.R
@@ -25,3 +25,31 @@ csvFile <- file.path(mainPath, fileName)
 
 ## Read CSV file
 cat(csvFile)
+
+
+## Fylke
+fylke <- data.table::fread("c:/enc/fylke_tbl1158.csv", fill = TRUE)
+dim(fylke)
+fylke
+
+## Kommune
+komm <- data.table::fread("c:/enc/kommune_tbl1160.csv", fill = TRUE)
+dim(komm)
+komm
+str(komm)
+summary(komm)
+
+## Bydeler
+bydel <- data.table::fread("c:/enc/bydel_tbl1168.csv", fill = TRUE)
+dim(bydel)
+bydel
+summary(bydel)
+
+## Grunnkrets fra SSB
+## Level 1 = delområder (noen steder er det lik bydeler f.eks Oslo, Rogaland, Vestland og Trondheim)
+## Level 2 = grunnkretser
+dt <- data.table::fread("c:/enc/enumerator2020.csv", fill = TRUE)
+dim(dt)
+dt
+summary(dt)
+dt[level == 1, ]
