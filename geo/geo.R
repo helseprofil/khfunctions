db055f527dadf2bb42776478147cbac9cb37265b
YBK <ybkamaleri@users.noreply.github.com>
2020-07-21 18:46:47 +0200

    Kommune fra grunnkrets ikke finnes i komme tbl
     Disse kommuekoder fra Grunnkrets tabell ikke finnes i kommue tabell 1850, 2100, 5012

--- geo/geo.R
+++ geo/geo.R
@@ -6,21 +6,33 @@ fylke <- fread("ssb_fylke.csv", fill = TRUE)
 
 ## Kommune
 kommune <- fread("ssb_kommune.csv", fill = TRUE)
-kommune[, fylkeCode := gsub("\\d{2}$", "", code)]
+kommune[, fylkeCode := as.numeric(gsub("\\d{2}$", "", code))]
 ## or stri_extract_first_regex(5644, '\\d{2}')
 setcolorder(kommune, c(9, 1:8))
 fwrite(kommune, "ssb_kommune02.csv", sep = ";")
 
 ## Bydels
 bydel <- fread("ssb_bydels.csv", fill = TRUE)
-bydel[, kommuneCode := gsub("\\d{2}$", "", code)]
+bydel[, kommuneCode := as.numeric(gsub("\\d{2}$", "", code))]
 dim(bydel)
 setcolorder(bydel, c(9, 1:8))
 fwrite(bydel, "ssb_bydels02.csv", sep = ";")
 
 ## Grunnkrets
 grunnkrets <- fread("ssb_grunnkrets.csv", fill = TRUE)
-grunnkrets[, kommuneCode := gsub("\\d{4}$", "", code)]
+grunnkrets[, kommuneCode := as.numeric(gsub("\\d{4}$", "", code))]
 dim(grunnkrets)
 setcolorder(grunnkrets, c(9, 1:8))
 fwrite(grunnkrets, "ssb_grunnkrets02.csv", sep = ";")
+
+
+## Kommune fra grunnkrets som ikke finnes i Kommune tabell
+## -------------------------------------------------------
+dk <- unique(grunnkrets$kommuneCode)
+kk <- unique(kommune$code)
+setdiff(dk, kk)
+## [1] 1850 2100 5012
+nokomm <- c(1850, 2100, 5012)
+grunnkrets[kommuneCode %in% nokomm, ]
+
+grunnkrets[name %like% "Tysfjord", ]
