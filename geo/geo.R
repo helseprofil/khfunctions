d6792596e0e160679714d81643ed4dd6dd7c9e3d
YBK <ybkamaleri@users.noreply.github.com>
2020-07-21 17:35:05 +0200

    Use ; as seperate

--- geo/geo.R
+++ geo/geo.R
@@ -9,18 +9,18 @@ kommune <- fread("ssb_kommune.csv", fill = TRUE)
 kommune[, fylkeCode := gsub("\\d{2}$", "", code)]
 ## or stri_extract_first_regex(5644, '\\d{2}')
 setcolorder(kommune, c(9, 1:8))
-fwrite(kommune, "ssb_kommune02.csv")
+fwrite(kommune, "ssb_kommune02.csv", sep = ";")
 
 ## Bydels
 bydel <- fread("ssb_bydels.csv", fill = TRUE)
 bydel[, kommuneCode := gsub("\\d{2}$", "", code)]
 dim(bydel)
 setcolorder(bydel, c(9, 1:8))
-fwrite(bydel, "ssb_bydels02.csv")
+fwrite(bydel, "ssb_bydels02.csv", sep = ";")
 
 ## Grunnkrets
 grunnkrets <- fread("ssb_grunnkrets.csv", fill = TRUE)
 grunnkrets[, kommuneCode := gsub("\\d{4}$", "", code)]
 dim(grunnkrets)
 setcolorder(grunnkrets, c(9, 1:8))
-fwrite(grunnkrets, "ssb_grunnkrets02.csv")
+fwrite(grunnkrets, "ssb_grunnkrets02.csv", sep = ";")
