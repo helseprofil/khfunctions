1ca30d372c261b8b020f44d60201d69191beb50a
YBK <ybkamaleri@users.noreply.github.com>
2020-07-21 19:43:21 +0200

    Change reference code

--- geo/geo.R
+++ geo/geo.R
@@ -9,6 +9,7 @@ kommune <- fread("ssb_kommune.csv", fill = TRUE)
 kommune[, fylkeCode := as.numeric(gsub("\\d{2}$", "", code))]
 ## or stri_extract_first_regex(5644, '\\d{2}')
 setcolorder(kommune, c(9, 1:8))
+setnames(kommune, "code", "kommuneCode")
 fwrite(kommune, "ssb_kommune02.csv", sep = ";")
 
 ## Bydels
@@ -16,6 +17,7 @@ bydel <- fread("ssb_bydels.csv", fill = TRUE)
 bydel[, kommuneCode := as.numeric(gsub("\\d{2}$", "", code))]
 dim(bydel)
 setcolorder(bydel, c(9, 1:8))
+setnames(bydel, "code", "bydelCode")
 fwrite(bydel, "ssb_bydels02.csv", sep = ";")
 
 ## Grunnkrets
@@ -23,6 +25,7 @@ grunnkrets <- fread("ssb_grunnkrets.csv", fill = TRUE)
 grunnkrets[, kommuneCode := as.numeric(gsub("\\d{4}$", "", code))]
 dim(grunnkrets)
 setcolorder(grunnkrets, c(9, 1:8))
+setnames(grunnkrets, "code", "grunnkretsCode")
 fwrite(grunnkrets, "ssb_grunnkrets02.csv", sep = ";")
 
 
