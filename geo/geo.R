339c19714b201270dfd98dc67b44ece2f8d1e775
Kamaleri <YusmanBin.Kamaleri@fhi.no>
2020-07-22 11:56:48 +0100

    Add solution to encoding problem

--- geo/geo.R
+++ geo/geo.R
@@ -2,6 +2,11 @@
 
 library(data.table)
 library(stringi)
+
+getOption("encoding")
+## obs! write.csv2 uses NA for missing while fwrite uses NULL
+## gives consequence when inporting to ACCESS or Excel
+
 fylke <- fread("ssb_fylke.csv", fill = TRUE)
 
 ## Kommune
@@ -11,6 +16,8 @@ kommune[, fylkeCode := as.numeric(gsub("\\d{2}$", "", code))]
 setcolorder(kommune, c(9, 1:8))
 setnames(kommune, "code", "kommuneCode")
 fwrite(kommune, "ssb_kommune02.csv", sep = ";")
+## write.csv2(kommune, "ssb_kommune03.csv", row.names = FALSE, fileEncoding = "native.enc")
+
 
 ## Bydels
 bydel <- fread("ssb_bydels.csv", fill = TRUE)
@@ -19,6 +26,7 @@ dim(bydel)
 setcolorder(bydel, c(9, 1:8))
 setnames(bydel, "code", "bydelCode")
 fwrite(bydel, "ssb_bydels02.csv", sep = ";")
+## write.csv2(bydel, "ssb_bydels03.csv", row.names = FALSE, fileEncoding = "Latin1")
 
 ## Grunnkrets
 grunnkrets <- fread("ssb_grunnkrets.csv", fill = TRUE)
@@ -27,15 +35,21 @@ dim(grunnkrets)
 setcolorder(grunnkrets, c(9, 1:8))
 setnames(grunnkrets, "code", "grunnkretsCode")
 fwrite(grunnkrets, "ssb_grunnkrets02.csv", sep = ";")
+## write.csv2(grunnkrets, "ssb_grunnkrets03.csv", row.names = FALSE, fileEncoding = "native.enc")
+
 
 
 ## Kommune fra grunnkrets som ikke finnes i Kommune tabell
 ## -------------------------------------------------------
 dk <- unique(grunnkrets$kommuneCode)
-kk <- unique(kommune$code)
+kk <- unique(kommune$kommuneCode)
 setdiff(dk, kk)
 ## [1] 1850 2100 5012
 nokomm <- c(1850, 2100, 5012)
 grunnkrets[kommuneCode %in% nokomm, ]
 
 grunnkrets[name %like% "Tysfjord", ]
+
+
+## Trouble with encoding in Windows and workaround is to open csv in Excel
+## then save.as Excel file before importing the Excel file in Access.
