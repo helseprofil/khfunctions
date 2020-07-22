5a3acf989437e7b8788f89e6556fd07ec8611a10
Kamaleri <YusmanBin.Kamaleri@fhi.no>
2020-07-22 12:32:10 +0100

    Use openxlsx pkg

--- geo/geo.R
+++ geo/geo.R
@@ -2,12 +2,17 @@
 
 library(data.table)
 library(stringi)
+library(openxlsx)
 
 getOption("encoding")
 ## obs! write.csv2 uses NA for missing while fwrite uses NULL
 ## gives consequence when inporting to ACCESS or Excel
+## or uses openxlsx package to create xlsx file
 
 fylke <- fread("ssb_fylke.csv", fill = TRUE)
+dim(fylke)
+setnames(fylke, "code", "fylkeCode")
+openxlsx::write.xlsx(fylke, "fylke.xlsx", colNames = TRUE)
 
 ## Kommune
 kommune <- fread("ssb_kommune.csv", fill = TRUE)
@@ -15,9 +20,10 @@ kommune[, fylkeCode := as.numeric(gsub("\\d{2}$", "", code))]
 ## or stri_extract_first_regex(5644, '\\d{2}')
 setcolorder(kommune, c(9, 1:8))
 setnames(kommune, "code", "kommuneCode")
+dim(kommune)
 fwrite(kommune, "ssb_kommune02.csv", sep = ";")
 ## write.csv2(kommune, "ssb_kommune03.csv", row.names = FALSE, fileEncoding = "native.enc")
-
+write.xlsx(kommune, "ssb_kommune.xlsx", colNames = TRUE)
 
 ## Bydels
 bydel <- fread("ssb_bydels.csv", fill = TRUE)
@@ -27,6 +33,8 @@ setcolorder(bydel, c(9, 1:8))
 setnames(bydel, "code", "bydelCode")
 fwrite(bydel, "ssb_bydels02.csv", sep = ";")
 ## write.csv2(bydel, "ssb_bydels03.csv", row.names = FALSE, fileEncoding = "Latin1")
+write.xlsx(bydel, "ssb_bydel.xlsx", colNames = TRUE)
+
 
 ## Grunnkrets
 grunnkrets <- fread("ssb_grunnkrets.csv", fill = TRUE)
@@ -36,7 +44,7 @@ setcolorder(grunnkrets, c(9, 1:8))
 setnames(grunnkrets, "code", "grunnkretsCode")
 fwrite(grunnkrets, "ssb_grunnkrets02.csv", sep = ";")
 ## write.csv2(grunnkrets, "ssb_grunnkrets03.csv", row.names = FALSE, fileEncoding = "native.enc")
-
+write.xlsx(grunnkrets, "ssb_grunnkrets.xlsx", colNames = TRUE)
 
 
 ## Kommune fra grunnkrets som ikke finnes i Kommune tabell
