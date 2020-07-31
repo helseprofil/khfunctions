8b0ca98d7064fc3602a504d06f133bba5d3da13a
Kamaleri <YusmanBin.Kamaleri@fhi.no>
2020-07-31 19:28:45 +0100

    Choose to include source file or not

--- test/testfile_chk.R
+++ test/testfile_chk.R
@@ -18,7 +18,7 @@ rdsPath <- normalizePath("C:\\enc\\DBtest\\PRODUKTER\\MELLOMPROD\\R\\STABLAORG\\
 ## Source Path
 srcPath <- normalizePath("F:\\Prosjekter\\Kommunehelsa\\PRODUKSJON\\ORGDATA\\UDIR\\ELEVUNDER\\ORG\\2017", "/")
 
-show_rds <- function(rdsPath, srcPath, rdsFile, srcFile){
+show_rds <- function(rdsPath, srcPath, rdsFile, srcFile, rds.only = FALSE){
 
   fileName <- file.path(rdsPath, rdsFile)
   dt <- readRDS(fileName)
@@ -28,14 +28,18 @@ show_rds <- function(rdsPath, srcPath, rdsFile, srcFile){
   subdt <- dt[rowSelect, ]
   geo <- unique(dt$GEOniv)
 
-  ## source file
-  fileExt <- tools::file_ext(srcFile)
-  srcName <- file.path(srcPath, srcFile)
-  srcDT <- switch(fileExt,
-                  "csv" = {data.table::fread(srcName)},
-                  "xlsx" = {readxl::read_excel(srcName)},
-                  {readxl::read_excel(srcName)} #default
-                  )
+  srcDT = NULL
+
+  if(rds.only == 0) {
+    ## source file
+    fileExt <- tools::file_ext(srcFile)
+    srcName <- file.path(srcPath, srcFile)
+    srcDT <- switch(fileExt,
+                    "csv" = {data.table::fread(srcName)},
+                    "xlsx" = {readxl::read_excel(srcName)},
+                    {readxl::read_excel(srcName)} #default
+                    )
+  }
 
   list(colnames = dtNames, geo = geo, dt = dt, src = srcDT)
 }
@@ -44,12 +48,13 @@ filNames <- c("ELEVUNDER.rds", "INNTULIKHET.rds")
 srcName <- "Elev_2015.csv"
 
 ## ELEVUNDER
-DT <- show_rds(rdsPath,
+show_rds(rdsPath, rdsFile = "ELEVUNDER.rds", rds.only = TRUE)
+elvDT <- show_rds(rdsPath,
                srcPath,
                rdsFile = "ELEVUNDER.rds",
                srcFile = "Elev_2015.csv")
 
-dt <- DT$dt
+dt <- elvDT$dt
 setDT(dt)
 names(dt)
 dt
@@ -67,11 +72,7 @@ dt[GEO == "1014", ]
 dt[GEO == "1804", ] #Bodo
 
 ## Source File
-srcPath <- "F:\\Prosjekter\\Kommunehelsa\\PRODUKSJON\\ORGDATA\\UDIR\\ELEVUNDER\\ORG\\2017"
-srcName <- "Elev_2015.csv"
-srcFile <- paste(srcPath, srcName, sep = "\\")
-
-DT <- fread(srcFile)
+DT <- elvDT$src
 names(DT)
 DT[GeografiId == "1014"]
 DT[GeografiId == "0101", ]
@@ -81,4 +82,25 @@ DT[GeografiId == "1804", ] #Bodø
 
 
 ## - INNTULIKHET
-DT <- show_rds(rdsPath, "INNTULIKHET.rds")
+## --------------
+srcPath = "F:\\Prosjekter\\Kommunehelsa\\PRODUKSJON\\ORGDATA\\SSB\\INNTULIKHET\\ORG\\P90P10\\2020"
+
+DT <- show_rds(rdsPath,
+               srcPath,
+               "INNTULIKHET.rds",
+               "18p9010byd_2009.csv")
+
+
+## source
+src <- DT$src
+head(src)
+src[, P9010 := sub('^(\\")(\\d\\,\\d)(\\")', "\\2", P9010)]
+src[, P9010 := sub("\\,", "\\.", P9010)]
+src
+
+
+sub("\\d+$", "\\1", "1234ab56")
+sub("\\d+", "\\1", "1234ab56")
+sub("\\D+", "\\1", "1234ab56")
+sub("\\D+\\d+", "\\1", "1234ab56")
+sub("\\D+", "_", "1234ab56")
