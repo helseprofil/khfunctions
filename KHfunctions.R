905dfcbe64d3717f3061eabfb564f25bf4665be5
Yusman Kamaleri <ybkamaleri@users.noreply.github.com>
2020-12-15 15:00:09 +0100

    Fix (quote): Raw data with quote
    Raw data with text format i.e KOBLID 22 Filgruppe ENEFHIM has opening
    and closing quote. Check RSYNT1 in INNLESING table how this is read.

--- KHfunctions.R
+++ KHfunctions.R
@@ -1366,12 +1366,7 @@ LesFil<-function (filbesk,batchdate=SettKHBatchDate(),globs=FinnGlobs(),dumps=ch
     names(DF)<-gsub("\\s$","",names(DF))
     names(DF)[names(DF)==""]<-paste("C",which(names(DF)==""),sep="")
     
-    ## These variables are to be use in Stata process (request from Jørgen)
-    ## They will be deleted when Stata RSYNT1 is completed below
-    DF$filgruppe <- filbesk$FILGRUPPE
-    DF$delid <- filbesk$DELID
-    DF$tab1_innles <- filbesk$TAB1
-
+   
     #DEV dette b?r v?re un?dvendig '' skal v?re lest inn som NA
     #DF[DF==""]<-NA
     #des<-lapply(DF,class)
@@ -1398,6 +1393,13 @@ LesFil<-function (filbesk,batchdate=SettKHBatchDate(),globs=FinnGlobs(),dumps=ch
       ok<-1
       if (grepl("<STATA>",synt)){
         synt<-gsub("<STATA>[ \n]*(.*)","\\1",synt)
+
+        ## These variables are to be use in Stata process (request from Jørgen)
+        ## They will be deleted when Stata RSYNT1 is completed below
+        DF$filgruppe <- filbesk$FILGRUPPE
+        DF$delid <- filbesk$DELID
+        DF$tab1_innles <- filbesk$TAB1
+        
         RES<-KjorStataSkript(DF,synt,batchdate=batchdate,globs=globs)
         if (RES$feil!=""){
           error<-paste("Noe gikk galt i kjøring av STATA",RES$feil,sep="\n")
@@ -1437,6 +1439,7 @@ LesFil<-function (filbesk,batchdate=SettKHBatchDate(),globs=FinnGlobs(),dumps=ch
   DF[c("filgruppe","delid","tab1_innles")] <- NULL
 
   TilFilLogg(filbesk$KOBLID,"TidLesFil",(proc.time()-klokke)[3],batchdate=batchdate,globs=globs)
+  
   default.stringsAsFactors=TRUE 
   return(list(DF=DF,ok=ok))
 }
