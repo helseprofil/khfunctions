e34021953caaeb60a5a8d2268aaf9f988a2f1ed3
YBK <=>
2022-09-13 13:08:08 +0200

    Misc: Comments

--- dev-sepaafil.R
+++ dev-sepaafil.R
@@ -10,11 +10,9 @@ k <- LagKUBE("RFU_NH_ROYK_5_fildumpdummy")
 
 
 
-
-
 ## stata kode for prikk
-<STATA>
-   include "F:\Forskningsprosjekter\PDB 2455 - Helseprofiler og til_\PRODUKSJON\BIN\Z_Statasnutter\Rsynt_Postprosess_naboprikking_del_1_LESEFERD_INNV.do"
-include "F:\Forskningsprosjekter\PDB 2455 - Helseprofiler og til_\PRODUKSJON\BIN\Z_Statasnutter\Rsynt_Postprosess_naboprikking_del_2_FELLES.do"
+## <STATA>
+## include "F:\Forskningsprosjekter\PDB 2455 - Helseprofiler og til_\PRODUKSJON\BIN\Z_Statasnutter\Rsynt_Postprosess_naboprikking_del_1_LESEFERD_INNV.do"
+## include "F:\Forskningsprosjekter\PDB 2455 - Helseprofiler og til_\PRODUKSJON\BIN\Z_Statasnutter\Rsynt_Postprosess_naboprikking_del_2_FELLES.do"
 
-* Ferdig
+## * Ferdig
