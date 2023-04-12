bc6d67aa529d869ca5a4238bb4b0700252e5728d
Vegard Lysne <vegard.lysne@fhi.no>
2023-04-12 09:47:56 +0200

    move debugging options

--- R/KHsetup.R
+++ R/KHsetup.R
@@ -14,13 +14,14 @@ library(data.table)
 library(readxl)
 library(fs)
 
+
+# Set debugging to inactive
+show_functions = FALSE
+show_arguments = FALSE
+
 # Load setup scripts
 kh_source(repo = "khfunctions", branch = "allviskube", file = "R/KHpaths.R", encoding = "latin1")
 kh_source(repo = "khfunctions", branch = "allviskube", file = "R/KHglobs.R", encoding = "latin1")
 kh_source(repo = "khfunctions", branch = "allviskube", file = "R/KHfunctions.R", encoding = "latin1")
 kh_source(repo = "khfunctions", branch = "allviskube", file = "R/KHkube.R", encoding = "latin1")
 
-
-# Set debugging to inactive
-show_functions = FALSE
-show_arguments = FALSE
