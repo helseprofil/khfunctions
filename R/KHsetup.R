03c76cf0a99144f4eaba4a5a32c7d8811a07166a
Vegard Lysne <vegard.lysne@fhi.no>
2023-04-13 10:09:51 +0200

    add debugging function to setup

--- R/KHsetup.R
+++ R/KHsetup.R
@@ -14,9 +14,39 @@ library(data.table)
 library(readxl)
 library(fs)
 
-# Load setup scripts (paths, global parameters, functions)
+# Define debugging function, set to inactive by default
+show_functions <- FALSE
+show_arguments <- FALSE
+
+#' is_kh_debug (ybk)
+#' 
+#' Debugging all other functions in the project
+#' To use, set show_functions or show_arguments = TRUE
+is_kh_debug <- function(fun = show_functions, arg = show_arguments, show = FALSE){
+  
+  # If both are TRUE then show_arguments will be deactivated automatically.
+  if (fun & arg)
+    arg <- FALSE
+  
+  if (arg) {
+    show = show_arguments
+    out = sys.calls()[[1]]
+  }
+  
+  if (fun) {
+    show = show_functions
+    out = sys.calls()[[1]][1]
+  }
+  
+  if (show) {
+    message("Execute: ", deparse(out))
+  }
+  
+  invisible()
+}
+
+# Load setup scripts (paths, global parameters, functions) to enable all functionality
 kh_source(repo = "khfunctions", branch = "allviskube", file = "R/KHpaths.R", encoding = "latin1")
 kh_source(repo = "khfunctions", branch = "allviskube", file = "R/KHglobs.R", encoding = "latin1")
 kh_source(repo = "khfunctions", branch = "allviskube", file = "R/KHfunctions.R", encoding = "latin1")
 kh_source(repo = "khfunctions", branch = "allviskube", file = "R/KHkube.R", encoding = "latin1")
-
