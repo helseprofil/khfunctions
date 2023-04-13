02b22e3e13af344e080c697cbb922e2f0cfee3df
Vegard Lysne <vegard.lysne@fhi.no>
2023-04-13 10:10:35 +0200

    add all helper functions needed for global parameters

--- R/KHglobs.R
+++ R/KHglobs.R
@@ -53,5 +53,268 @@ globglobs <- list(
   XLScols = as.vector(sapply(c("", as.vector(paste(sapply(c("", LETTERS[]), paste, LETTERS[], sep = "")))), paste, LETTERS[], sep = ""))
 )
 
+# Setter standard designegenskaper, slik som delenes kolonnenavn og status i omkoding
+# Se tabell KH_DELER
+SettDefDesignKH <- function(globs = FinnGlobs()) {
+  is_kh_debug()
+  
+  Deler <- sqlQuery(globs$dbh, "SELECT * FROM KH_DELER", as.is = TRUE, stringsAsFactors = FALSE)
+  # DelKols<-lapply(as.list(setNames(Deler$DelKols, Deler$DEL)),function(x){unlist(str_split(x,pattern=","))})
+  # Tilrettelegging for enkle oppslag:
+  DelKolN <- setNames(Deler$DelKol, Deler$DEL)
+  DelKolE <- setNames(Deler$DelKolE, Deler$DEL)
+  DelType <- setNames(Deler$TYPE, Deler$DEL)
+  DelFormat <- setNames(Deler$FORMAT, Deler$DEL)
+  AggPri <- Deler$DEL[order(Deler$AGGREGERPRI)]
+  AggVedStand <- Deler$DEL[Deler$AGGREGERvedPRED == 1]
+  IntervallHull <- setNames(Deler$INTERVALLHULL, Deler$DEL)
+  IntervallHull <- IntervallHull[!(is.na(IntervallHull) | IntervallHull == "")]
+  
+  DelKols <- as.list(DelKolN)
+  DelKolsF <- DelKols
+  KolsDel <- list()
+  for (del in names(DelKols)) {
+    if (DelType[del] == "INT") {
+      DelKols[[del]] <- paste(DelKols[[del]], c("l", "h"), sep = "")
+      DelKolsF[[del]] <- DelKols[[del]]
+    }
+    if (!(is.na(DelKolE[[del]]) | DelKolE[[del]] == "")) {
+      DelKolsF[[del]] <- c(DelKolsF[[del]], unlist(str_split(DelKolE[[del]], ",")))
+    }
+    for (kol in DelKols[[del]]) {
+      KolsDel[[kol]] <- del
+    }
+  }
+  
+  
+  UBeting <- Deler$DEL[Deler$OMKODbet == "U"]
+  BetingOmk <- Deler$DEL[Deler$OMKODbet == "B"]
+  BetingF <- Deler$DEL[Deler$OMKODbet == "F"]
+  OmkDel <- c(UBeting, BetingOmk)
+  # IntervallHull<-list(A="DekkInt/TotInt>0.999 | (NTOT>=10 & NHAR/NTOT>0.8) | (TotInt<=20 & DekkInt>=10) | TotInt<=10")
+  
+  DesignKols <- c(unlist(DelKols[c(UBeting, BetingOmk)]))
+  DesignKolsF <- c(DesignKols, unlist(DelKols[BetingF]))
+  DesignKolsFA <- c(DesignKolsF, setdiff(unlist(DelKolsF[c(UBeting, BetingOmk)]), unlist(DelKols[c(UBeting, BetingOmk)])))
+  
+  
+  
+  return(
+    list(
+      DelKols = DelKols,
+      DelKolsF = DelKolsF,
+      KolsDel = KolsDel,
+      DelKolN = DelKolN,
+      DelType = DelType,
+      DelFormat = DelFormat,
+      UBeting = UBeting,
+      BetingOmk = BetingOmk,
+      BetingF = BetingF,
+      OmkDel = OmkDel,
+      DesignKols = DesignKols,
+      DesignKolsF = DesignKolsF,
+      DesignKolsFA = DesignKolsFA,
+      AggPri = AggPri,
+      AggVedStand = AggVedStand,
+      IntervallHull = IntervallHull,
+      AMissAllow = TRUE
+    )
+  )
+}
+
+SettKodeBokGlob <- function(globs = FinnGlobs()) {
+  is_kh_debug()
+  
+  OmkodD <- sqlQuery(globs$dbh, "SELECT * FROM KH_OMKOD
+                            UNION SELECT ID, DEL, KODE as NYKODE, KODE as ORGKODE, 0 as PRI_OMKOD, 1 AS OBLIG FROM KH_KODER", as.is = TRUE, stringsAsFactors = FALSE)
+  KB <- list()
+  
+  for (del in names(globs$DefDesign$DelKolN)) {
+    KBD <- subset(OmkodD, DEL == del)
+    if (globs$DefDesign$DelType[del] == "INT") {
+      if (nrow(KBD) > 0) {
+        KBD[, c("ORGKODEl", "ORGKODEh", "NYKODEl", "NYKODEh")] <- as.integer(NA)
+      } else {
+        KBD <- cbind(KBD, data.frame(ORGKODEl = integer(0), ORGKODEh = integer(0), NYKODEl = integer(0), NYKODEh = integer(0)))
+      }
+    } else if (globs$DefDesign$DelFormat[del] == "integer") {
+      KBD$ORGKODE <- as.integer(KBD$ORGKODE)
+      KBD$NYKODE <- as.integer(KBD$NYKODE)
+    }
+    kbdnames <- names(KBD)
+    kbdnames <- gsub("ORGKODE", globs$DefDesign$DelKolN[del], kbdnames)
+    kbdnames <- gsub("NYKODE(h|l|)", paste(globs$DefDesign$DelKolN[del], "\\1_omk", sep = ""), kbdnames)
+    kbdnames <- gsub("NYKODE(h|l|)", paste(globs$DefDesign$DelKolN[del], "\\1_omk", sep = ""), kbdnames)
+    kbdnames <- gsub("PRI_OMKOD", paste(del, "_pri", sep = ""), kbdnames)
+    kbdnames <- gsub("OBLIG", paste(del, "_obl", sep = ""), kbdnames)
+    setnames(KBD, names(KBD), kbdnames)
+    KB[[del]] <- KBD[, names(KBD)[!names(KBD) %in% c("ID", "DEL")]]
+  }
+  return(KB)
+}
+
+SettLegitimeKoder <- function(globs = FinnGlobs()) {
+  is_kh_debug()
+  
+  Koder <- sqlQuery(globs$dbh, "SELECT * FROM KH_KODER", as.is = TRUE, stringsAsFactors = FALSE)
+  KodeL <- list()
+  for (del in unique(Koder$DEL)) {
+    KodeD <- subset(Koder, DEL == del)
+    if (globs$DefDesign$DelType[del] == "INT") {
+      KodeD <- cbind(KodeD, setNames(matrix(as.integer(str_split_fixed(KodeD$KODE, "_", 2)), ncol = 2), globs$DefDesign$DelKols[[del]]))
+    } else if (globs$DefDesign$DelFormat[del] == "integer") {
+      KodeD <- setNames(cbind(KodeD, as.integer(KodeD$KODE)), c(names(KodeD), globs$DefDesign$DelKols[[del]]))
+    } else if (globs$DefDesign$DelFormat[del] == "character") {
+      KodeD <- setNames(cbind(KodeD, KodeD$KODE), c(names(KodeD), globs$DefDesign$DelKols[[del]]))
+    }
+    KodeL[[del]] <- KodeD
+  }
+  return(KodeL)
+}
+
+SettTotalKoder <- function(globs = FinnGlobs()) {
+  is_kh_debug()
+  
+  Koder <- sqlQuery(globs$dbh, "SELECT KH_KODER.DEL,KODE, FORMAT FROM KH_KODER INNER JOIN KH_DELER ON KH_KODER.DEL=KH_DELER.DEL WHERE TOTAL=1", as.is = TRUE, stringsAsFactors = FALSE)
+  TotKoder <- list()
+  for (del in Koder$DEL) {
+    if (Koder$FORMAT[Koder$DEL == del] == "integer") {
+      TotKoder[[del]] <- as.integer(Koder$KODE[Koder$DEL == del])
+    } else {
+      TotKoder[[del]] <- Koder$KODE[Koder$DEL == del]
+    }
+  }
+  return(TotKoder)
+}
+
+## Find the most recent version of locally installed Stata
+FinnStataExe <- function() {
+  stata_bin <- "StataSE-64.exe"
+  program_path <- c("C:/Program Files/", "C:/Program Files (x86)/")
+  stata_prog <- grep("Stata", fs::dir_ls(program_path), value = TRUE)
+  stata_ver <- stringi::stri_extract(stata_prog, regex = "\\d{2}$")
+  Vers <- max(as.numeric(stata_ver))
+  stata_path <- grep(Vers, stata_prog, value = TRUE)
+  Exe <- file.path(stata_path, stata_bin)
+  
+  return(list(Exe = Exe, Vers = Vers))
+}
 
 
+SettGlobs <- function(path = "", modus = NA, gibeskjed = FALSE) {
+  is_kh_debug()
+  
+  # Setter globale parametre
+  # Disse er faste over hver kjøring og endres normalt bare ved systemoppdatering/-migrering
+  # Merk at globs$dbh ikke lukkes av seg selv, dermed kan det bli rot med gamle slike om FinnGlobs brukes for mye
+  # Bruk evy odbcCloseAll() for å rydde absolutt alle
+  
+  # Les globglobs (se topp av fil)
+  globs <- globglobs
+  if (is.na(modus)) {
+    modus <- globs$HOVEDmodus
+  }
+  
+  if (modus == "KH") {
+    globs$KubeDir <- globs$KubeDir_KH
+    globs$KubeDirNy <- globs$KubeDirNy_KH
+    globs$KubeDirDat <- globs$KubeDirDat_KH
+  } else {
+    globs$KubeDir <- globs$KubeDir_NH
+    globs$KubeDirNy <- globs$KubeDirNy_NH
+    globs$KubeDirDat <- globs$KubeDirDat_NH
+  }
+  
+  KHdbname <- globs$KHdbname
+  # Sett path om denne ikker er oppgitt:
+  
+  if (path == "") {
+    if (file.exists(paste(getwd(), KHdbname, sep = "/"))) {
+      path <- getwd()
+      if (gibeskjed == TRUE) {
+        cat("Setter path = ", path, " fra getwd()\n")
+      }
+    } else {
+      i <- 1
+      while (path == "" & i <= length(defpaths)) {
+        if (file.exists(paste(defpaths[i], KHdbname, sep = "/"))) {
+          path <- defpaths[i]
+          cat("Setter path=", path, "fra defpaths\n")
+        }
+        i <- i + 1
+      }
+    }
+    if (path == "") {
+      cat(globs$stjstr, "******KRITISK FEIL: path ikke funnet\n******Har du tilgang til F:/?", globs$stjstr, sep = "")
+    }
+  } else if (!file.exists(paste(path, KHdbname, sep = "/"))) {
+    cat(globs$stjstr, "******KRITISK FEIL: path har ikke hovedfila", KHdbname, globs$stjstr, sep = "")
+    path <- ""
+  }
+  
+  if (path != "") {
+    
+    ## Use other location of KHELSA.mdb and KHlogg.mdb
+    ## This is needed due to constant crash with unstable network
+    dbFile <- globs$KHdbname
+    logFile <- globs$KHlogg
+    
+    if (exists("setLocalPath", envir = .GlobalEnv)) {
+      path <- setLocalPath
+    }
+    
+    # Sys.getenv("R_ARCH")   gir "/x64"eller "/i386"
+    KHOc <- odbcConnectAccess2007(paste(path, dbFile, sep = "/"))
+    # KHOc<-odbcConnectAccess(paste(path,KHdbname,sep="/"))
+    KHLc <- odbcConnectAccess2007(paste(path, logFile, sep = "/"))
+    
+    ## OBS!! Have to reset path to original for all the other globs use
+    path <- rawPath
+  }
+  
+  globs <- c(globs, list(dbh = KHOc, log = KHLc, path = path))
+  
+  GeoNavn <- data.table(sqlQuery(KHOc, "SELECT * from GeoNavn", as.is = TRUE))
+  GeoKoder <- data.table(sqlQuery(KHOc, "SELECT * from GEOKoder", as.is = TRUE), key = c("GEO"))
+  UtGeoKoder <- GeoKoder[TYP == "O"]$GEO
+  KnrHarm <- data.table(sqlQuery(KHOc, "SELECT * from KnrHarm", as.is = TRUE), key = c("GEO"))
+  TKNR <- data.table(sqlQuery(KHOc, "SELECT * from TKNR", as.is = TRUE), key = c("ORGKODE"))
+  HELSEREG <- data.table(sqlQuery(KHOc, "SELECT * from HELSEREG", as.is = TRUE), key = c("FYLKE"))
+  # Gjelder også for soner
+  KnrHarmS <- lapply(KnrHarm[, c("GEO", "GEO_omk"), with = FALSE], function(x) {
+    paste(x, "00", sep = "")
+  })
+  KnrHarmS <- cbind(as.data.frame(KnrHarmS, stringsAsFactors = FALSE), HARMstd = KnrHarm$HARMstd)
+  KnrHarm <- rbind(KnrHarm, KnrHarmS)
+  # Må legge til de som ikke omkodes for å lette bruk i merge
+  # KnrHarm<-rbind(KnrHarm,data.frame(KNRorg=GeoKoder$GEO[TIL<2008],KNRharm=GeoKoder$GEO[TIL<2008],HARMstd=2008))
+  
+  
+  # GK til bydel. Bør konsolideres med KnrHarm
+  GkBHarm <- data.table(sqlQuery(KHOc, "SELECT * FROM GKBydel2004T", as.is = TRUE), key = c("GK,Bydel2004"))
+  
+  globs$DefDesign <- SettDefDesignKH(globs = globs)
+  globs$KB <- SettKodeBokGlob(globs = globs)
+  globs$LegKoder <- SettLegitimeKoder(globs = globs)
+  globs$TotalKoder <- SettTotalKoder(globs = globs)
+  Stata <- FinnStataExe()
+  globs$StataExe <- Stata$Exe
+  globs$StataVers <- Stata$Vers
+  return(c(globs, list(GeoNavn = GeoNavn, GeoKoder = GeoKoder, UtGeoKoder = UtGeoKoder, KnrHarm = KnrHarm, GkBHarm = GkBHarm, TKNR = TKNR, HELSEREG = HELSEREG)))
+}
+
+# Definer KHglobs
+KHglobs <- SettGlobs()
+
+FinnGlobs <- function() {
+  is_kh_debug()
+  
+  # Hjelperutine, kjekk å brukes som default. Bruker KHglobs eller setter denne
+  globs <- NA
+  if (exists("KHglobs")) {
+    globs <- KHglobs
+  } else {
+    globs <- SettGlobs()
+  }
+  return(globs)
+}
