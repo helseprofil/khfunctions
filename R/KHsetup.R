55b1c58d89a0781fa5610aab093d152cdd9a4007
Vegard Lysne <vegard.lysne@fhi.no>
2024-02-09 14:37:58 +0100

    move .updatelockfile() to KHsetup.R

--- R/KHsetup.R
+++ R/KHsetup.R
@@ -3,6 +3,40 @@ if(version$major >= 4 & version$minor >= 2){
   Sys.setlocale("LC_ALL", "nb-NO.UTF-8")
 }
 
+#' .updatelockfile (VL)
+#' Helper function to automatically fetch the lastest renv.lock from github
+.updatelockfile <- function(){
+  
+  # Check if the master branch is active
+  b <- system("git branch --show-current", intern = TRUE)
+  if(b != "master"){
+    message("You are not on the master branch, keep on the good dev work!")
+    return(invisible(NULL))
+  }
+  
+  # Check if an update is necessary
+  tryCatch({
+    new_lockfile <- readLines("https://raw.githubusercontent.com/helseprofil/khfunctions/master/renv.lock")
+    if (identical(readLines("renv.lock"), new_lockfile)) {
+      return(invisible(NULL))
+    } else {
+      choice <- menu(choices = c("Yes", "No"), 
+                     title = "renv.lock update available. Overwrite local file?")
+      if (choice == 1) {
+        writeLines(new_lockfile, "renv.lock") 
+        message("renv.lock updated, update to latest package versions.")
+        renv::restore()
+      } else {
+        message("Skipping renv.lock update, your package versions differ from the project.")
+      }
+    }
+  }, error = function(e) {
+    message("Could not fetch renv.lock from the repository. Check URL and connectivity.")
+  })
+}
+
+.updatelockfile()
+
 # Load packages
 library(conflicted)
 library(epitools) # Egentlig ikke i bruk, bare i direkte_stdz_jome.R
@@ -42,5 +76,3 @@ source("https://raw.githubusercontent.com/helseprofil/khfunctions/master/R/KHfil
 source("https://raw.githubusercontent.com/helseprofil/khfunctions/master/R/KHkubefunctions.R", encoding = "latin1")
 source("https://raw.githubusercontent.com/helseprofil/khfunctions/master/R/KHkube.R", encoding = "latin1")
 source("https://raw.githubusercontent.com/helseprofil/khfunctions/master/R/KHother.R", encoding = "latin1")
-
-
