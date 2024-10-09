0149e46b2bc35e0e5712d3b17e2bdafdbc5d20c2
Vegard <vegard.lysne@helsedir.no>
2024-10-09 15:25:14 +0200

    fix links

--- .Rprofile
+++ .Rprofile
@@ -1,3 +1,1 @@
-source("renv/activate.R")
-
 source("https://raw.githubusercontent.com/helseprofil/khfunctions/master/R/KHsetup.R")
--- R/KHsetup.R
+++ R/KHsetup.R
@@ -35,11 +35,11 @@ show_functions <- FALSE
 show_arguments <- FALSE
 
 # Load setup scripts (paths, global parameters, functions) to enable all functionality
-source("https://raw.githubusercontent.com/helseprofil/khfunctions/production-test/R/KHmisc.R", encoding = "latin1")
-source("https://raw.githubusercontent.com/helseprofil/khfunctions/production-test/R/KHpaths.R", encoding = "latin1")
-source("https://raw.githubusercontent.com/helseprofil/khfunctions/production-test/R/KHglobs.R", encoding = "latin1")
-source("https://raw.githubusercontent.com/helseprofil/khfunctions/production-test/R/KHfilgruppefunctions.R", encoding = "latin1")
-source("https://raw.githubusercontent.com/helseprofil/khfunctions/production-test/R/KHfilgruppe.R", encoding = "latin1")
-source("https://raw.githubusercontent.com/helseprofil/khfunctions/production-test/R/KHkubefunctions.R", encoding = "latin1")
-source("https://raw.githubusercontent.com/helseprofil/khfunctions/production-test/R/KHkube.R", encoding = "latin1")
-source("https://raw.githubusercontent.com/helseprofil/khfunctions/production-test/R/KHother.R", encoding = "latin1")
+source("https://raw.githubusercontent.com/helseprofil/khfunctions/master/R/KHmisc.R", encoding = "latin1")
+source("https://raw.githubusercontent.com/helseprofil/khfunctions/master/R/KHpaths.R", encoding = "latin1")
+source("https://raw.githubusercontent.com/helseprofil/khfunctions/master/R/KHglobs.R", encoding = "latin1")
+source("https://raw.githubusercontent.com/helseprofil/khfunctions/master/R/KHfilgruppefunctions.R", encoding = "latin1")
+source("https://raw.githubusercontent.com/helseprofil/khfunctions/master/R/KHfilgruppe.R", encoding = "latin1")
+source("https://raw.githubusercontent.com/helseprofil/khfunctions/master/R/KHkubefunctions.R", encoding = "latin1")
+source("https://raw.githubusercontent.com/helseprofil/khfunctions/master/R/KHkube.R", encoding = "latin1")
+source("https://raw.githubusercontent.com/helseprofil/khfunctions/master/R/KHother.R", encoding = "latin1")
